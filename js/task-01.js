c3a74f737b8c1f35d51c0b081369dcc5fc5b6241
AMIHbKA <bishop0013forum@gmail.com>
2023-02-02 23:14:32 +0200

    create

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const listCategoriesEl = document.getElementById('categories');
+
+
+
+console.log(listCategoriesEl.childElementCount);
+
+// console.log(`Number of categories: ${listCategoriesEl.children.length}`);
+
+const itemCategoriesListEl = listCategoriesEl.querySelectorAll('.item');
+console.log(itemCategoriesListEl)
