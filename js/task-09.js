90a1eca6eef72a8607c9f0f59cb53b35e46bfb00
AMIHbKA <bishop0013forum@gmail.com>
2023-02-05 07:00:16 +0200

    tsk#9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const btnChangeColorRef = document.querySelector(".change-color");
+const colorValueRef = document.querySelector(".color");
+
+function onBtnChangeColorClickHandler(event) {
+  colorValueRef.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = colorValueRef.textContent;
+}
+
+btnChangeColorRef.addEventListener("click", onBtnChangeColorClickHandler);
