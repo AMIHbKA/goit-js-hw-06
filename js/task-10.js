150f6f7d5a08b543e1f3effc8468f706f6545bef
AMIHbKA <bishop0013forum@gmail.com>
2023-02-05 13:35:18 +0200

    tsk#10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector("#controls input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  targetDiv: document.getElementById("boxes"),
+};
+
+function createBoxes(amount) {
+  const elem = [];
+  let size = 20;
+  const targetDiv = document.getElementById("boxes");
+
+  for (let i = 0; i < amount; i += 1) {
+    size += 10;
+    const curEl = document.createElement("div");
+    curEl.style.width = `${size}px`;
+    curEl.style.height = `${size}px`;
+    curEl.style.backgroundColor = getRandomHexColor();
+    elem.push(curEl);
+  }
+
+  targetDiv.append(...elem);
+}
+
+function destroyBoxes() {
+  refs.targetDiv.innerHTML = "";
+}
+
+function onBtnCreateClickHandler() {
+  createBoxes(refs.input.value);
+}
+
+refs.btnCreate.addEventListener("click", onBtnCreateClickHandler);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
